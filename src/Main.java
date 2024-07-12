8032aca56aeda5dd153fb74d8554938a48e32f07
Nemanja Tripic <tripicnemanja96@gmail.com>
2024-07-12 19:59:02 +0200

    - Dodavanje koda koji ucitava podatke iz fajla prevozna_sredstva.csv i smjesta te podatke u odgovarajuce objekte klasa

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,58 @@
+import org.unibl.etf.vozila.ElektricniAutomobil;
+import org.unibl.etf.vozila.ElektricniBicikl;
+import org.unibl.etf.vozila.ElektricniTrotinet;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
 
+        ArrayList<ElektricniAutomobil> automobili = new ArrayList<>();
+        ArrayList<ElektricniBicikl> bicikli = new ArrayList<>();
+        ArrayList<ElektricniTrotinet> trotineti = new ArrayList<>();
+        Set<String> sviJedinstveniId = new HashSet<>();
+
         try {
             BufferedReader in = new BufferedReader(new FileReader("prevozna_sredstva.csv"));
             String s;
+            in.readLine();
             while ((s = in.readLine()) != null) {
+
+                // parsiranje fajla
                 String[] karakteristikeVozila = s.split(",");
-                for (int i = 0; i < karakteristikeVozila.length-1; i++) {
-                    if(karakteristikeVozila[i].isEmpty()){
-                        System.out.print("X");
+                String jedinstveniIdenitifikator = karakteristikeVozila[0];
+                String proizvodjac = karakteristikeVozila[1];
+                String model = karakteristikeVozila[2];
+                String datumNabavke = karakteristikeVozila[3];
+                String cijena = karakteristikeVozila[4];
+                String domet = karakteristikeVozila[5];
+                String maksimalnaBrzina = karakteristikeVozila[6];
+                String opis = karakteristikeVozila[7];
+                String vrsta =  karakteristikeVozila[8];
+                // //
+
+                // prvo provjera da li je input sa istim ID vec ucitan
+                if(!sviJedinstveniId.contains(jedinstveniIdenitifikator)){
+                    sviJedinstveniId.add(jedinstveniIdenitifikator);
+
+                    if('A' == jedinstveniIdenitifikator.charAt(0)){
+                        automobili.add(new ElektricniAutomobil(proizvodjac,
+                                Double.parseDouble(cijena), proizvodjac, model,
+                                100, datumNabavke, opis));
+                    } else if('B' == jedinstveniIdenitifikator.charAt(0)){
+                        bicikli.add(new ElektricniBicikl(jedinstveniIdenitifikator, Double.parseDouble(cijena),
+                                proizvodjac, model, 100, Integer.parseInt(domet)));
+                    } else if('T' == jedinstveniIdenitifikator.charAt(0)){
+                        trotineti.add(new ElektricniTrotinet(jedinstveniIdenitifikator, Double.parseDouble(cijena),
+                                proizvodjac, model, 100, Integer.parseInt(maksimalnaBrzina)));
                     }
-                    System.out.printf("%-20s", karakteristikeVozila[i]);
                 }
-                System.out.println();
+
             }
+
+
         in.close();
         } catch (Exception e) {
             e.printStackTrace(); // treba dodati neki specificni exception, a ne samo genericki Exception
