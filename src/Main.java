7f3798e54638fc6f265bc7cee14a23b0242dc680
Nemanja Tripic <tripicnemanja96@gmail.com>
2024-07-12 16:12:16 +0200

    -Dodavanje fajla sa ulaznim podacima o prevoznim sredstvima
    -Dodavanje testnog koda sa ucitavanje podataka iz fajla i njihovo ispisivanje na konzolu

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,25 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class Main {
     public static void main(String[] args) {
 
-
+        try {
+            BufferedReader in = new BufferedReader(new FileReader("prevozna_sredstva.csv"));
+            String s;
+            while ((s = in.readLine()) != null) {
+                String[] karakteristikeVozila = s.split(",");
+                for (int i = 0; i < karakteristikeVozila.length-1; i++) {
+                    if(karakteristikeVozila[i].isEmpty()){
+                        System.out.print("X");
+                    }
+                    System.out.printf("%-20s", karakteristikeVozila[i]);
+                }
+                System.out.println();
+            }
+        in.close();
+        } catch (Exception e) {
+            e.printStackTrace(); // treba dodati neki specificni exception, a ne samo genericki Exception
+        }
+    }
 }
