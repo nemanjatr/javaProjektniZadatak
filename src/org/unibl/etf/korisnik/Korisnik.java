0206231c172282289616b9386502ae6c1a9faf3b
Nemanja Tripic <tripicnemanja96@gmail.com>
2024-07-13 15:45:22 +0200

    Dodavnje klase koja apstrakuje korisnika prevoznog sredstva

--- src/org/unibl/etf/korisnik/Korisnik.java
+++ src/org/unibl/etf/korisnik/Korisnik.java
@@ -1,2 +1,9 @@
-package org.unibl.etf.korisnik;public class Korisnik {
+package org.unibl.etf.korisnik;
+
+public class Korisnik {
+    private String brojLicneKarte;
+    private String brojPasosa;
+    private String brojVozackeDozvole;
+    private boolean imaVozackuDozvolu;
+    private boolean jesteStraniDrzavljanin;
 }
