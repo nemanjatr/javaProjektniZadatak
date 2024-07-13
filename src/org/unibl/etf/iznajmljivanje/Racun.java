5d865c4bbd71b21ee22012c2cfad0b2f030396b2
Nemanja Tripic <tripicnemanja96@gmail.com>
2024-07-13 18:37:21 +0200

    - Dodavanje racuna kao sto je definisano tekstom zadatka koji cuva informacije o iznosu i ukupnom placanju za pojedinacna iznajmljivanja

--- src/org/unibl/etf/iznajmljivanje/Racun.java
+++ src/org/unibl/etf/iznajmljivanje/Racun.java
@@ -1,2 +1,40 @@
-package org.unibl.etf.iznajmljivanje;public class Racun {
+package org.unibl.etf.iznajmljivanje;
+
+public class Racun {
+    private int osnovnaCijena;
+    private int iznos;
+    private int ukupnoZaPlacanje;
+
+    public Racun(){
+
+    }
+
+    public Racun(int osnovnaCijena, int ukupnoZaPlacanje){
+        this.osnovnaCijena = osnovnaCijena;
+        this.ukupnoZaPlacanje = ukupnoZaPlacanje;
+    }
+
+    public int getOsnovnaCijena() {
+        return osnovnaCijena;
+    }
+
+    public void setOsnovnaCijena(int osnovnaCijena) {
+        this.osnovnaCijena = osnovnaCijena;
+    }
+
+    public int getUkupnoZaPlacanje() {
+        return ukupnoZaPlacanje;
+    }
+
+    public void setUkupnoZaPlacanje(int ukupnoZaPlacanje) {
+        this.ukupnoZaPlacanje = ukupnoZaPlacanje;
+    }
+
+    public int getIznos() {
+        return iznos;
+    }
+
+    public void setIznos(int iznos) {
+        this.iznos = iznos;
+    }
 }
