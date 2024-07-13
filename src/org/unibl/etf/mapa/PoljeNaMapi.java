cc58f37ba8609cd49f4e303dcacce85b4fa0194a
Nemanja Tripic <tripicnemanja96@gmail.com>
2024-07-13 15:46:29 +0200

    Dodavanje klase koja predstavlja jedno polje na mapi sa koordinatama x i y kao svojim vrijednostima

--- src/org/unibl/etf/mapa/PoljeNaMapi.java
+++ src/org/unibl/etf/mapa/PoljeNaMapi.java
@@ -1,2 +1,33 @@
-package org.unibl.etf.mapa;public class PoljeNaMapi {
+package org.unibl.etf.mapa;
+
+public class PoljeNaMapi {
+    private int koordinataX;
+    private int koordinataY;
+
+    public PoljeNaMapi(int koordinataX, int koordinataY){
+        this.koordinataX = koordinataX;
+        this.koordinataY = koordinataY;
+    }
+
+    public int getKoordinataY() {
+        return koordinataY;
+    }
+
+    public int getKoordinataX() {
+        return koordinataX;
+    }
+
+    public void setKoordinataX(int koordinataX) {
+        this.koordinataX = koordinataX;
+    }
+
+    public void setKoordinataY(int koordinataY) {
+        this.koordinataY = koordinataY;
+    }
+
+    @Override
+    public String toString(){
+        return "(" + koordinataX + ", " + koordinataY + ")";
+    }
 }
+
