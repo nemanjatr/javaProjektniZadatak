c21aa4d41c41e1c1b5377cf6b8221b069b257fd7
Nemanja Tripic <tripicnemanja96@gmail.com>
2024-07-13 18:38:09 +0200

    - Dodavanje koda u klasu Iznajmljivanje za rad sa racunom

--- src/org/unibl/etf/iznajmljivanje/Iznajmljivanje.java
+++ src/org/unibl/etf/iznajmljivanje/Iznajmljivanje.java
@@ -5,23 +5,47 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class Iznajmljivanje {
+
+    public static final int DISTANCE_NARROW = 5;
+    public static final int DISTANCE_WIDE = 10;
+    public static final double DISCOUNT = 0.1;
+    public static final double DISCOUT_PROM = 0.2;
+    public static final int CAR_UNIT_PRICE = 3;
+    public static final int BIKE_UNIT_PRICE = 2;
+    public static final int SCOOTER_UNIT_PRICE = 1;
+
+    private static int brojacInstanci = 0;
+    private int redniBrojIznajmljivanja;
+
     private LocalDateTime  datumVrijeme;
     private String imeKorisnika;
+
+    // obratiti paznju jer ovo polje nije navedeno u tekstu zadatka, ali ja mislim da je neophodno
+    private String identifikatorPrevoznogSredstva;
+
     private PoljeNaMapi pocetnaLokacija;
     private PoljeNaMapi krajnjaLokacija;
     private int trajanjeVoznjeSekunde;
     private boolean desioSeKvar;
     private boolean imaPromociju;
 
-    //private Racun racunZaPlacanje;
+    private String tarifaNaplacivanja;
+    private Racun racunZaPlacanje;
 
 
-    public Iznajmljivanje(String datumVrijeme, String imeKorisnika, String pocetnaLokacija, String krajnjaLokacija,
-                          String trajanjeVoznjeSekunde, String desioSeKvar, String imaPromociju){
+    public Iznajmljivanje(String datumVrijeme, String imeKorisnika, String identifikatorPrevoznogSredstva, String pocetnaLokacija,
+                          String krajnjaLokacija, String trajanjeVoznjeSekunde, String desioSeKvar, String imaPromociju){
+
+        this.brojacInstanci++;
+        this.redniBrojIznajmljivanja = brojacInstanci;
 
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d.M.yyyy H:mm");
         this.datumVrijeme = LocalDateTime.parse(datumVrijeme.trim(), formatter);
         this.imeKorisnika = imeKorisnika;
+
+        //
+        this.identifikatorPrevoznogSredstva = identifikatorPrevoznogSredstva;
+        //
 
         //
 
@@ -37,6 +61,64 @@ public class Iznajmljivanje {
         this.trajanjeVoznjeSekunde = Integer.parseInt(trajanjeVoznjeSekunde);
         this.desioSeKvar = desioSeKvar.equalsIgnoreCase("da");
         this.imaPromociju = imaPromociju.equalsIgnoreCase("da");
+        this.racunZaPlacanje = new Racun();
+    }
+
+    public void racunanjeOsnovneCijene() {
+        if('A' == identifikatorPrevoznogSredstva.charAt(0)) {
+            racunZaPlacanje.setOsnovnaCijena((int)CAR_UNIT_PRICE * trajanjeVoznjeSekunde);
+        } else if('B' == identifikatorPrevoznogSredstva.charAt(0)) {
+            racunZaPlacanje.setOsnovnaCijena((int)BIKE_UNIT_PRICE * trajanjeVoznjeSekunde);
+        } else if('T' == identifikatorPrevoznogSredstva.charAt(0)) {
+            racunZaPlacanje.setOsnovnaCijena((int)SCOOTER_UNIT_PRICE * trajanjeVoznjeSekunde);
+        } else {
+            System.out.println("Pogresan jedinstveni identifikator vozila u podacima za iznajmljivanje");
+        }
+    }
+
+    public void racunanjeTarifeNaplacivanja(){
+        if(pocetnaLokacija.getUziDioGrada()) {
+            tarifaNaplacivanja = "uzi";
+        } else {
+            tarifaNaplacivanja = "siri";
+        }
+    }
+
+    public void racunanjeIznosa() {
+        int osnovnaCijena = racunZaPlacanje.getOsnovnaCijena();
+        if("uzi".equals(tarifaNaplacivanja)) {
+            racunZaPlacanje.setIznos(DISTANCE_NARROW * osnovnaCijena);
+        } else if("siri".equals(tarifaNaplacivanja)) {
+            racunZaPlacanje.setIznos(DISTANCE_WIDE * osnovnaCijena);
+        } else {
+            System.out.println("Greska u vrijednosti tarife naplacivanja. Ne odgovara ni tarifi za uzi dio grada niti za siri!");
+        }
+    }
+
+    public void racunanjeUkupnogPlacanja() {
+        int iznos = racunZaPlacanje.getIznos();
+        int iznosPromocije = 0;
+        int iznosPopusta = 0;
+        if(desioSeKvar) {
+            racunZaPlacanje.setUkupnoZaPlacanje(0);
+        } else {
+            if(redniBrojIznajmljivanja % 10 == 0) {
+                iznosPopusta = (int) (DISCOUNT * iznos);
+            }
+            if(imaPromociju) {
+                iznosPromocije = (int) (DISCOUT_PROM * iznos);
+            }
+            racunZaPlacanje.setUkupnoZaPlacanje(iznos - iznosPopusta - iznosPromocije);
+        }
+    }
+
+    public void generisiRacun(){
+        this.racunanjeOsnovneCijene();
+        this.racunanjeTarifeNaplacivanja();
+        this.racunanjeIznosa();
+        this.racunanjeUkupnogPlacanja();
+        System.out.println("Racun za " + redniBrojIznajmljivanja + " iznajmljivanje: " + "\t\n" +
+                "     * ukupno za placanje " + racunZaPlacanje.getUkupnoZaPlacanje());
     }
 
     @Override
