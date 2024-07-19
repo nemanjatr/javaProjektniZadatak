176eca506188554fdfaa4686849085c3d06d410b
Nemanja Tripic <tripicnemanja96@gmail.com>
2024-07-19 20:42:56 +0200

    - Dodavanje klase Mapa sa jednim statickim poljem mapa, koje predstavlja nasu mapu iz simulacije
     - Unutar klase kao osnovna verzija dodat je jednostavan konstruktor koji kreira mapu

--- src/org/unibl/etf/mapa/Mapa.java
+++ src/org/unibl/etf/mapa/Mapa.java
@@ -1,2 +1,19 @@
-package org.unibl.etf.mapa;public class Mapa {
+package org.unibl.etf.mapa;
+
+public class Mapa {
+
+    // mozda bi trebalo da bude SINGLETON pattern, ali za sada neka ostane ovako
+
+    private static final int VELICINA_MAPE = 20;
+    public static PoljeNaMapi[][] mapa = new PoljeNaMapi[VELICINA_MAPE][VELICINA_MAPE];
+
+    public Mapa() {
+        for(int i = 0; i < VELICINA_MAPE; i++) {
+            for(int j = 0; j < VELICINA_MAPE; j++) {
+                mapa[i][j]  = new PoljeNaMapi(i, j);
+            }
+        }
+    }
+
+
 }
