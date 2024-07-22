1f2132d03c94d459c10c91db2d02d7b34c6cb316
Nemanja Tripic <tripicnemanja96@gmail.com>
2024-07-22 21:21:50 +0200

    - Izmjena u sihronizaciji niti iznajmljivanja tako se konkurentno izvrsavaju niti sa istim datuom i vremenom, a ostale da cekaju i tako redom
     - To je uradjeno tako sto se u Map mapiraju sva iznajmljivanja sa istim datuom i vremenom, a onda se odatle formira ArrayLista grupisana po datumu i vremenu
    
     - Konacno prolaskom kroz svaku listu startuju se samo niti sa istim dat-vrijeme i ceka se da one zavrse da bi se pokrenule nove

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,12 @@ import org.unibl.etf.iznajmljivanje.Iznajmljivanje;
 import org.unibl.etf.mapa.Mapa;
 import org.unibl.etf.vozila.PrevoznoSredstvo;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -16,19 +22,52 @@ public class Main {
 
 
         eMobilityCompany.ucitajIznajmljivanjaIzFajla();
-        for(Iznajmljivanje i : eMobilityCompany.getIznajmljivanja()) {
-            //System.out.println(i);
-            i.start();
+        Map<LocalDateTime, List<Iznajmljivanje>> grupisanoPoDatumVrijeme =
+                eMobilityCompany.getIznajmljivanja().stream().collect(Collectors.groupingBy(Iznajmljivanje::getDatumVrijeme));
+
+        ArrayList<ArrayList<Iznajmljivanje>> listaIznajmljivanjaPoDatumVrijeme = new ArrayList<>();
+        for(List<Iznajmljivanje> grupa : grupisanoPoDatumVrijeme.values()) {
+            listaIznajmljivanjaPoDatumVrijeme.add(new ArrayList<>(grupa));
         }
 
-        for(Iznajmljivanje i : eMobilityCompany.getIznajmljivanja()) {
-            //System.out.println(i);
-            try{
-                i.join();
+        for(ArrayList<Iznajmljivanje> podlista : listaIznajmljivanjaPoDatumVrijeme) {
+            System.out.println("Grupa " + podlista.getFirst().getDatumVrijeme());
+
+            for(Iznajmljivanje i : podlista) {
+                //System.out.println(i);
+                i.start();
+            }
+
+            try {
+                for(Iznajmljivanje i : podlista) {
+                    i.join();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("------------------------------------");
+            try {
+                Thread.sleep(5000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
+
+
+//        for(Iznajmljivanje i : eMobilityCompany.getIznajmljivanja()) {
+//            //System.out.println(i);
+//            i.start();
+//        }
+
+//        for(Iznajmljivanje i : eMobilityCompany.getIznajmljivanja()) {
+//            //System.out.println(i);
+//            try{
+//                i.join();
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        }
 
 
 
