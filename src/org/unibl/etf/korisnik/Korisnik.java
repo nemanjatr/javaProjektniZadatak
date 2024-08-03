4e5b154219f8cd844bafc33ab3c8a9c43ea55a5c
Nemanja Tripic <tripicnemanja96@gmail.com>
2024-08-03 20:51:00 +0200

    - Izmjene u klasi Korisnik, dodavanje toString i konsktruktora

--- src/org/unibl/etf/korisnik/Korisnik.java
+++ src/org/unibl/etf/korisnik/Korisnik.java
@@ -1,9 +1,29 @@
 package org.unibl.etf.korisnik;
 
 public class Korisnik {
+    String imeKorisnika;
     private String brojLicneKarte;
     private String brojPasosa;
     private String brojVozackeDozvole;
     private boolean imaVozackuDozvolu;
     private boolean jesteStraniDrzavljanin;
+
+    public Korisnik(String imeKorisnika) {
+        this.imeKorisnika = imeKorisnika;
+
+    }
+
+    public String getImeKorisnika() {
+        return imeKorisnika;
+    }
+
+    @Override
+    public String toString() {
+        String stringZaIspis = "";
+        String idDokument = (jesteStraniDrzavljanin) ? "pasos: " + brojPasosa + " (strani drzavljanin)" : "licna karta: " + brojLicneKarte;
+        String vozackaDozvola = (imaVozackuDozvolu) ? "vozacka dozvola" + brojVozackeDozvole : "nema vozacku dozvolu";
+
+        return "Korisnik " + imeKorisnika + " " + idDokument + vozackaDozvola;
+    }
+
 }
