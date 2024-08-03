73f497c25a4948979d8d818efbd88f845d4279a8
Nemanja Tripic <tripicnemanja96@gmail.com>
2024-08-03 20:54:45 +0200

    - Testiranje novih izmjena u main metodi

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,7 @@
 import org.unibl.etf.iznajmljivanje.EMobilityCompany;
 import org.unibl.etf.iznajmljivanje.Iznajmljivanje;
 import org.unibl.etf.mapa.Mapa;
-import org.unibl.etf.vozila.PrevoznoSredstvo;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
-import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
@@ -16,6 +9,16 @@ public class Main {
         EMobilityCompany eMobilityCompany = EMobilityCompany.getInstanca();
         Mapa mapa = new Mapa();
         eMobilityCompany.obaviIznajmljivanja();
+
+
+
+
+        for(Iznajmljivanje i : eMobilityCompany.izvrsenaIznajmljivanja) {
+            System.out.println(i);
+        }
+
+        //eMobilityCompany.prikazSumarnogIzvjestaja();
+        eMobilityCompany.prikazDnevnihIzvjestaja();
 
 
 
